efb61a1c86d1211ff64b83d9cb67a04d1e393533
danielrodcaball <danielrodcaball@users.noreply.github.com>
2025-02-16 00:45:41 +0000

    chore(docs): Generated API Reference (v2)

--- www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
+++ www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
@@ -4,8 +4,8 @@ curl -X POST '{backend_url}/admin/orders/{id}/fulfillments/{fulfillment_id}/ship
 --data-raw '{
   "items": [
     {
-      "id": "id_uHIzYcTxCHG",
-      "quantity": 3083340957614080
+      "id": "id_bm9SARmyGHErjFWEJ48",
+      "quantity": 8952774551142400
     }
   ],
   "metadata": {}
